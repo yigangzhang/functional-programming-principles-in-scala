f2d6f2bae0a03405dfbee2f0ee8f2faab6816917
Shuffle <samzhangcode@gmail.com>
2018-06-08 22:16:28 -0400

    implemented google and apple tweet. Finished week3

--- week3-data-and-abstraction/objsets/src/main/scala/objsets/TweetSet.scala
+++ week3-data-and-abstraction/objsets/src/main/scala/objsets/TweetSet.scala
@@ -222,14 +222,15 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  lazy val allTweets = TweetReader.allTweets
+  lazy val googleTweets: TweetSet = allTweets.filter(t => google.exists(t.text.contains))
+  lazy val appleTweets: TweetSet = allTweets.filter(t => apple.exists(t.text.contains))
 
   /**
     * A list of all tweets mentioning a keyword from either apple or google,
     * sorted by the number of retweets.
     */
-  lazy val trending: TweetList = ???
+  lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
 }
 
 object Main extends App {
